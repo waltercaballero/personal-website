06266202b7c0a9d5e5f5dc36fe6930479ec10893
Walter Caballero <waltercaballero@gmail.com>
2026-03-01 23:51:49 -0300

    update - install pagespeed

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import "./style.css";
 import { inject } from "@vercel/analytics";
+import { injectSpeedInsights } from "@vercel/speed-insights";
 
-// Inicializa Vercel Analytics
 inject();
+injectSpeedInsights();
